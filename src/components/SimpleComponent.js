c0952efedd8174fb57e8b899fc6ea947d7690207
Ray Blasko <mray6288@gmail.com>
2018-04-24 17:07:22 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,21 @@
-// Code SimpleComponentHere Here
+import React from 'react'
+
+export default class SimpleComponent extends React.Component {
+
+	constructor () {
+		super()
+
+		this.state = { mood:'happy'}
+	}
+
+	changeMood = () => {
+		this.state.mood === 'happy' ? this.setState({mood:'sad'}) : this.setState({mood:'happy'})
+	}
+
+	render() {
+		return <div onClick={this.changeMood}>
+				{this.state.mood}
+				</div>
+	}
+
+}
