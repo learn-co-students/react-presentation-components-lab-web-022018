ee2877373b0cc37e3cd59ea7325e4d122c2192a6
bafab <bafa@tutanota.de>
2018-04-25 19:51:57 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -2,16 +2,14 @@ import React from 'react'
 export default class SimpleComponent extends React.Component {
   constructor() {
     super()
-    this.state = {mood:'happy'}
+    this.state = {mood: 'happy'}
   }
 
   change = () => {
-    this.state.mood === 'happy' ? this.setState({mood:'sad'}) : this.setState({mood:'happy'})
+    this.state.mood === 'happy' ? this.setState({mood: 'sad'}) : this.setState({mood: 'happy'})
   }
 
   render() {
-    return (
-      <div onClick={this.change}>{this.state.mood}</div>
-    )
+    return <div onClick={this.change}>{this.state.mood}</div>
   }
 }
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,6 +1,6 @@
 import React from 'react'
-const SimplerComponent = (props) =>  {
+const SimplerComponent = (props) => {
   return <div onClick={props.handleClick}>I am just happy</div>
-}
+  }
 
 export default SimplerComponent
