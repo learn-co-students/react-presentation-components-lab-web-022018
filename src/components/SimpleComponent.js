1a1007c6f862ba46fd58ff050d03e7d40e19df2b
vsparrow <khan@vsparrow.com>
2018-04-25 23:58:04 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,24 @@
 // Code SimpleComponentHere Here
+import React from 'react'
+
+class SimpleComponent extends React.Component{
+  constructor(props){
+    super(props)
+    console.log(props);
+    this.state = {mood: "happy"}
+  }
+  handleClick = ()=>{
+    const mood = this.state.mood === 'happy' ? 'sad' : 'happy'
+    this.setState({mood: mood})
+  }
+  render(){
+    return(
+      <div onClick={this.handleClick}>{this.state.mood}</div>
+    ) //return
+  }//render
+}//end class
+
+
+
+
+export default SimpleComponent
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,24 @@
-// Code SimplerComponentHere Here
+// In the components/SimplerComponent.js file, create a SimplerComponent component.
+// The component should be a "stateless functional" component.
+// It should render a <div> to the page that contains the text: "I am just happy".
+// It should receive one property called handleClick that performs some sort of action
+//  in response to a click — your choice!
+
+// const defaultLimit = 100
+// const TextField = (props) =>
+//   <input
+//     className="field field-light"
+//     onChange={props.onChange}
+//     limit={props.limit || defaultLimit}
+//   />;
+import React from 'react'
+
+{/*
+  is return implicit only for x=> y not x=>{y} ??
+  
+*/}
+const SimplerComponent  = (props) => {
+
+  return <div onClick={props.handleClick}>  I am just happy  </div>
+}
+export default SimplerComponent
